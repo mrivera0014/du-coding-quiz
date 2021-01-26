c96c313c0850a0316996f0af0448a659d0927c7b
Marble <mrivera0014@gmail.com>
2021-01-25 21:46:39 -0700

    psuedo coded js file, declared variables, listed functions needed

---
+++ script.js
@@ -0,0 +1,50 @@
+// Variables that I will need
+
+var time =
+
+var button = document.getElementById("start")
+
+var playerScore =
+
+var correct = true
+
+var questions {
+    qOne = "Some question"
+    qTwo = "Some question"
+    qThree = "Some question"
+    qFour = "Some question"
+}
+
+//FUNCTIONS I WILL NEED
+
+//a timer to be set after the start button has been clicked-eventListener timerInterval setInterval 
+function setTimer() {
+
+}
+
+//at the same time I need the first question to appear when the start button has been pressed-
+
+function
+
+
+
+    //as the user selects their answers I need 2 things to happen
+
+    function
+
+    //FIRST- if the selected answer is wrong the time needs to decrease by x amount of seconds
+
+    //once the player has answered all questions I need their score
+
+    score = "endTime"
+
+//once we have their score we can send a message thy player their score
+
+function sendMessage() { }
+
+function
+    //SECOND- as the answers are selected I need the next question to appear
+
+    //to set everything off I need the first event to happen
+
+    setTimer()
